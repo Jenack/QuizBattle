32bd125870c12170bca2c1bfe3bda99b160e17f2
Nicholas [Home PC] <king-devlin@hotmail.com>
2016-03-28 23:38:59 +0200

    Setup for Flag Quiz Game

---
+++ Assets/Scripts/Flags.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flags : MonoBehaviour {
+
+
+    public GameObject[] flagAnwsers;
+    public string[] flagArray;
+    
+	// Use this for initialization
+	void Start () {
+        //sorts them wierdly :p 3,4,1,2?? :P
+        //flagAnwsers = GameObject.FindGameObjectsWithTag("Flag");
+
+        for (int i = 0; i < flagAnwsers.Length; i++)
+        {
+            Debug.Log("Flag Number " + i + " is named " + flagAnwsers[i].name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
