0f7fcd193e3cc1c8c8360f17b752383032c10b34
Nicholas <king-devlin@hotmail.com>
2016-03-31 18:25:07 +0200

    Flag game logic works, now just needs point system

--- Assets/Scripts/Flags.cs
+++ Assets/Scripts/Flags.cs
@@ -1,32 +1,126 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Linq; //Needed??
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Flags : MonoBehaviour {
+
+    public GameObject[] flagButton; //Makes a box in Unity Editor so we can drag and drop the elemen we need
+    public string[] flagNameArray; //Visual array to check the county names are loaded correctly
+    public Sprite[] allFlags; //Visual array to check the flags are loaded correctly
+    public Text QuestionText; //Makes a box in Unity Editor so we can drag and drop the element we need
+
+    private List<int> flagCheck = new List<int>(); //Creates a list for the purpose of tracking our randomly selected flags, to make sure that a flag can only be selected once
+    private int CorrectFlag;
+
+    //Function to load a text file which contains the countries of the flags we have
+    void LoadCountries() {
+        TextAsset textFile = Resources.Load("Countries") as TextAsset;
+        //Debug.Log(textFile); //Used to debug if file loaded or not 
+        flagNameArray = textFile.text.Split('\n');
+    }
     
+    void LoadFlags() {
+        //Load the Sprites folder from the Resources folder
+        Sprite[] imports = Resources.LoadAll<Sprite>("Sprites");
+        //Array to store the flag sprites
+        allFlags = new Sprite[imports.Length];
 
-    public GameObject[] flagAnwsers;
-    public string[] flagNameArray;
-    public Sprite[] allFlags;
-   
-	// Use this for initialization
-	void Start () {
-	    Object[] imports = (Sprite[]); Resources.LoadAll("Assets/Sprites/*.png");
-
-        allFlags = new Sprite[allFlags.Length];
-        
         for (var i = 0 ; i < allFlags.Length; i++) {
             allFlags[i] = imports[i];
         }
-        //sorts them wierdly :p 3,4,1,2?? :P
-        //flagAnwsers = GameObject.FindGameObjectsWithTag("Flag");
+    }
+    
+    void PlaceFlags() {
 
-        for (int i = 0; i < flagAnwsers.Length; i++)
+        for (int i = 0; i < flagButton.Length; i++)
         {
-            Debug.Log("Flag Number " + i + " is named " + flagAnwsers[i].name);
+            //Flag indexes, tells us which flags we have in our questions
+            int randomFlagIndex = Random.Range(0, allFlags.Length);
+            while(flagCheck.Contains(randomFlagIndex)){
+                randomFlagIndex = Random.Range(0, allFlags.Length);
+            }
+            flagCheck.Add(randomFlagIndex);
+            //used to debug that the corresponding flag contains the correct country name 
+            //Debug.Log("Flag Number " + i + " is named " + flagButton[i].name);
+            flagButton[i].GetComponent<Image>().sprite = allFlags[randomFlagIndex];
+            flagButton[i].GetComponent<MaterialUI.SpriteSwapper>().sprite1x = allFlags[randomFlagIndex];
+            flagButton[i].GetComponent<MaterialUI.SpriteSwapper>().sprite2x = allFlags[randomFlagIndex];
+            flagButton[i].GetComponent<MaterialUI.SpriteSwapper>().sprite4x = allFlags[randomFlagIndex];
         }
+
+        CorrectFlag = Random.Range(0, 4);
+        //Random flag index
+        QuestionText.text = "Which flag is "+flagNameArray[flagCheck[CorrectFlag]]+"s?";
+    }
+
+    public void ClickFlag1() {
+        if (CorrectFlag == 0)
+        {
+            Debug.Log("Correct Flag " + flagNameArray[flagCheck[CorrectFlag]]);
+            flagCheck.Clear();
+            PlaceFlags();
+        }
+        else
+        {
+            flagCheck.Clear();
+            PlaceFlags();
+        }
+    }
+
+    public void ClickFlag2()
+    {
+        if (CorrectFlag == 1)
+        {
+            Debug.Log("Correct Flag " + flagNameArray[flagCheck[CorrectFlag]]);
+            flagCheck.Clear();
+            PlaceFlags();
+        }
+        else
+        {
+            flagCheck.Clear();
+            PlaceFlags();
+        }
+    }
+
+    public void ClickFlag3()
+    {
+        if (CorrectFlag == 2)
+        {
+            Debug.Log("Correct Flag " + flagNameArray[flagCheck[CorrectFlag]]);
+            flagCheck.Clear();
+            PlaceFlags();
+        }
+        else {
+            flagCheck.Clear();
+            PlaceFlags();
+        }
+    }
+
+    public void ClickFlag4()
+    {
+        if (CorrectFlag == 3)
+        {
+            Debug.Log("Correct Flag " + flagNameArray[flagCheck[CorrectFlag]]);
+            flagCheck.Clear();
+            PlaceFlags();
+        }
+        else
+        {
+            flagCheck.Clear();
+            PlaceFlags();
+        }
+    }
+    
+    // Use this for initialization
+	void Start () {
+        
+        LoadFlags();
+        LoadCountries();
+        PlaceFlags();
+              
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
