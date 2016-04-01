3ddaacda8c65a8e7322f765a6b34ad488ff6ef36
Nicholas [Home PC] <king-devlin@hotmail.com>
2016-04-01 15:39:28 +0200

    Code base setup for CORRECT and FALSE panel for UI feedback

--- Assets/Scripts/Flags.cs
+++ Assets/Scripts/Flags.cs
@@ -9,9 +9,19 @@ public class Flags : MonoBehaviour {
     public string[] flagNameArray; //Visual array to check the county names are loaded correctly
     public Sprite[] allFlags; //Visual array to check the flags are loaded correctly
     public Text QuestionText; //Makes a box in Unity Editor so we can drag and drop the element we need
+    public GameObject True;
+    public GameObject False;
 
     private List<int> flagCheck = new List<int>(); //Creates a list for the purpose of tracking our randomly selected flags, to make sure that a flag can only be selected once
     private int CorrectFlag;
+
+    // Use this for initialization
+    void Start()
+    {
+        LoadFlags();
+        LoadCountries();
+        PlaceFlags();
+    }
 
     //Function to load a text file which contains the countries of the flags we have
     void LoadCountries() {
@@ -54,75 +64,97 @@ public class Flags : MonoBehaviour {
         QuestionText.text = "Which flag is "+flagNameArray[flagCheck[CorrectFlag]]+"s?";
     }
 
+    IEnumerator FlagTimer(){
+        //print(Time.time);
+        yield return new WaitForSeconds(5);
+        //print(Time.time);
+    } 
+
     public void ClickFlag1() {
         if (CorrectFlag == 0)
         {
             Debug.Log("Correct Flag " + flagNameArray[flagCheck[CorrectFlag]]);
+            True.SetActive(true);
+            StartCoroutine(FlagTimer());
+            True.SetActive(false);
             flagCheck.Clear();
             PlaceFlags();
+            StopCoroutine(FlagTimer());
         }
         else
         {
+            False.SetActive(true);
+            StartCoroutine(FlagTimer());
+            False.SetActive(false);
             flagCheck.Clear();
             PlaceFlags();
+            StopCoroutine(FlagTimer());
         }
     }
 
-    public void ClickFlag2()
-    {
+    public void ClickFlag2() {
         if (CorrectFlag == 1)
         {
             Debug.Log("Correct Flag " + flagNameArray[flagCheck[CorrectFlag]]);
+            True.SetActive(true);
+            StartCoroutine(FlagTimer());
+            True.SetActive(false);
             flagCheck.Clear();
             PlaceFlags();
+            StopCoroutine(FlagTimer());
         }
         else
         {
+            False.SetActive(true);
+            StartCoroutine(FlagTimer());
+            False.SetActive(false);
             flagCheck.Clear();
             PlaceFlags();
+            StopCoroutine(FlagTimer());
         }
     }
 
-    public void ClickFlag3()
-    {
+    public void ClickFlag3() {
         if (CorrectFlag == 2)
         {
             Debug.Log("Correct Flag " + flagNameArray[flagCheck[CorrectFlag]]);
+            True.SetActive(true);
+            StartCoroutine(FlagTimer());
+            True.SetActive(false);
             flagCheck.Clear();
             PlaceFlags();
-        }
-        else {
-            flagCheck.Clear();
-            PlaceFlags();
-        }
-    }
-
-    public void ClickFlag4()
-    {
-        if (CorrectFlag == 3)
-        {
-            Debug.Log("Correct Flag " + flagNameArray[flagCheck[CorrectFlag]]);
-            flagCheck.Clear();
-            PlaceFlags();
+            StopCoroutine(FlagTimer());
         }
         else
         {
+            False.SetActive(true);
+            StartCoroutine(FlagTimer());
+            False.SetActive(false);
             flagCheck.Clear();
             PlaceFlags();
+            StopCoroutine(FlagTimer());
         }
     }
-    
-    // Use this for initialization
-	void Start () {
-        
-        LoadFlags();
-        LoadCountries();
-        PlaceFlags();
-              
-	}
 
-	// Update is called once per frame
-	void Update () {
-
-	}
+    public void ClickFlag4() {
+        if (CorrectFlag == 3)
+        {
+            Debug.Log("Correct Flag " + flagNameArray[flagCheck[CorrectFlag]]);
+            True.SetActive(true);
+            StartCoroutine(FlagTimer());
+            True.SetActive(false);
+            flagCheck.Clear();
+            PlaceFlags();
+            StopCoroutine(FlagTimer());
+        }
+        else
+        {
+            False.SetActive(true);
+            StartCoroutine(FlagTimer());
+            False.SetActive(false);
+            flagCheck.Clear();
+            PlaceFlags();
+            StopCoroutine(FlagTimer());
+        }
+    }
 }
