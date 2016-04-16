d4360e1678d0aa86bf34d93e657eb33337b13523
Nicholas [Home PC] <king-devlin@hotmail.com>
2016-04-16 16:14:10 +0200

    Alpha 1.0

--- Assets/SVGs/ConvertSVGs.bat
+++
@@ -1,3 +0,0 @@
-:: Requires ImageMagick
-:: http://www.imagemagick.org/Usage/basics/#mogrify
-mogrify -format png *.svg
--- Assets/Scripts/Question.cs
+++
@@ -1,8 +0,0 @@
-﻿
-[System.Serializable]
-public class Question {
-
-    public string fact;
-    public bool isTrue;
-
-}
